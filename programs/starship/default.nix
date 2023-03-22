57a666a539874e73d19244ed4d98362ccbd3925f
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2023-03-22 01:56:31 +0100

    updates

--- programs/starship/default.nix
+++ programs/starship/default.nix
@@ -2,7 +2,6 @@
   programs.starship = {
     enable = true;
     settings = {
-      format = "$battery$username$hostname$directory$git_branch$git_status$git_state$cmd_duration";
       right_format = "$time";
       add_newline = false;
       git_branch = {
@@ -11,11 +10,6 @@
       cmd_duration = {
         format = "[$duration]($style) ";
         style = "yellow";
-      };
-      battery = {
-        full_symbol = "•";
-        charging_symbol = "⇡";
-        discharging_symbol = "⇣";
       };
       time = {
         disabled = false;
